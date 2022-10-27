f617f07a8331ebff514f091ff50c15122be76f81
Marco Aurélio Ghelere <cau.ghelere@hotmail.com>
2022-10-26 22:05:59 -0300

    Added fetch to login menu

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+function login(event){
+    //Codigo da funcao fetch, obtida de: 
+    //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_json_data
 
+    event.preventDefault() //previne o botao submit de recarregar a pagina
 
+    const data = {
+        "email": document.getElementById("email-input").value,
+        "password": document.getElementById("password-input").value,
+    };
+
+    //"email": "eve.holt@reqres.in",
+    //"password": "cityslicka"
+
+    fetch('https://reqres.in/api/login', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+            
+            if(!data.error){
+                //Caso nao haja erro no login, ira aparecer a barra de pesquisa:
+                document.getElementById("searchbar").classList.add("turnvisible");
+                localStorage.setItem("token", data.token);
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+        
+}
+
+if(localStorage.getItem("token") != null) {
+    console.log(document.getElementById("searchbar"));
+    document.getElementById("searchbar").classList.add("turnvisible");
+}
 
 // const xhr = new XMLHttpRequest();
 //     xhr.open("POST", "https://reqres.in/api/login", true);
